5f4719e4a139605ddd4bdc602023772d7ef0efff
slavegod <theokrator@proton.me>
2025-01-02 15:19:00 -0800

    Update aimbot-rework.lua

--- aimbot-rework.lua
+++ aimbot-rework.lua
@@ -1,1 +1,17 @@
 
+--[[
+    reworked aimbot by krigsprofitör (1afbb8d916894a4ca5af2ac53615f576)
+    created for https://discord.gg/ZTkMV9yD
+]]
+
+-- silence unknown constants for vsc
+local getgenv
+
+getgenv().aimbot_config = {
+    aimbot_settings = {
+        
+    },
+}
+
+-- improve functionality?
+loadstring(game:HttpGet("https://raw.githubusercontent.com/slavegod/roblox-scripts/refs/heads/main/src/aimbot-rework.lua"))()
